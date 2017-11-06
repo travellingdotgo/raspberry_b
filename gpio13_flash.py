979ad72e9d62b263e5eda585577521d4ebdb1308
c <c@g.hk>
2017-11-06 22:01:36 +0800

    update

---
+++ gpio13_flash.py
@@ -0,0 +1,14 @@
+
+# -*- coding: utf-8 -*-
+import RPi.GPIO as GPIO
+import time
+# BOARD编号方式，基于插座引脚编号
+GPIO.setmode(GPIO.BOARD)
+# 输出模式
+GPIO.setup(13, GPIO.OUT)
+
+while True:
+    GPIO.output(13, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(13, GPIO.LOW)
+    time.sleep(1)
